c98c9edae6d8735ec9012185ef1c5d4525d3d210
Enrriquep3 <enrriquep3@gmail.com>
2021-11-24 14:40:48 -0500

    ga

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { hydrate, render } from 'react-dom';
 import App from './App';
+
+ReactGA.initialize('G-MZL4V1N70Z'); // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 // See https://reactjs.org/docs/strict-mode.html
 const StrictApp = () => (
