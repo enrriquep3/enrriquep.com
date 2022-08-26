3b76e4e55e2cb16f7ea4da3cac2be35a8cc8a24d
Enrrique Pimentel <enrriquep3@gmail.com>
2022-08-26 09:07:39 -0600

    new changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,6 @@ const data = [
     subtitle: 'Sushi restaurant website',
     link: 'https://sushiplace-v1.vercel.app/',
     image: '/images/projects/sushiplace.jpg',
-    date: '2020-06-28',
     desc:
     'This is a sushi restaurant website with menu, offers, and social-contact page. ',
   },
@@ -13,7 +12,6 @@ const data = [
     subtitle: 'Youtube',
     link: 'https://youtubeapp-ten.vercel.app/',
     image: '/images/projects/youtubes.jpg',
-    date: '2020-11-20',
     desc:
       'This app allows you to watch YouTube videos, search for any video on demand ',
   },
@@ -22,7 +20,6 @@ const data = [
     subtitle: 'City Poltrait and Real Time Weather',
     link: 'https://enrriquep3.github.io/PROJECT-01/',
     image: '/images/projects/poltrait.jpg',
-    date: '2020-03-15',
     desc:
       'With this app you can find any city live weather and a great poltrait of the city just by typing the name of the city and clicking search ',
   },
@@ -31,7 +28,6 @@ const data = [
     subtitle: 'Search Movies',
     link: 'https://moviesearch-gamma.vercel.app/',
     image: '/images/projects/moviesearch.jpg',
-    date: '2020-09-20',
     desc:
       'This app allows you to search any movie, see the description, rating, date of release, and where to watch it. ',
   },
